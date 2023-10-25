be6305b996070c6d676942c1653941d053260050
Marcos Pereira <vinicius.hein@gmail.com>
2023-10-25 15:43:04 -0300

    feat: add colors with context

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,17 +2,19 @@ export type ButtonProps = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className }: ButtonProps) => {
-  return (
+const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
+  const generalStyles = "rounded-md px-6 py-2";
+  const Btn = (classes: string) => (
     <button
-      className={`
-        bg-primary rounded-md px-6 py-2 text-white
-        ${className}
-    `}
+      {...rest}
+      className={`${classes} ${generalStyles} ${className}`}
+      disabled={disabled}
     >
       {children}
     </button>
   );
+
+  return Btn(disabled ? "bg-disabled text-disabled" : "bg-primary text-white");
 };
 
 export default Button;
