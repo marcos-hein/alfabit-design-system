45d4e6d47254f633d82e25837db276ca29c66f20
Marcos Pereira <vinicius.hein@gmail.com>
2023-10-24 22:38:51 -0300

    feat: creating design tokes

---
+++ components/Button/Button.tsx
@@ -0,0 +1,18 @@
+export type ButtonProps = {
+  children: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({ children, className }: ButtonProps) => {
+  return (
+    <button
+      className={`
+        bg-primary rounded-md px-6 py-2 text-white
+        ${className}
+    `}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
