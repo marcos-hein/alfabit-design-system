473202df65dc554a935fe803badd3559af7ffc96
Marcos Pereira <vinicius.hein@gmail.com>
2023-10-25 16:51:33 -0300

    feat: add secondary button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,20 +1,46 @@
 export type ButtonProps = {
   children: React.ReactNode;
+  variant?: "primary" | "secondary" | "tertiary";
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
-  const generalStyles = "rounded-md px-6 py-2";
-  const Btn = (classes: string) => (
+function getVariant(
+  variant: ButtonProps["variant"],
+  disabled: ButtonProps["disabled"]
+) {
+  switch (variant) {
+    case "primary":
+      return disabled ? "bg-disabled text-disabled" : "bg-primary text-white";
+    case "secondary":
+      return disabled
+        ? "bg-disabled text-disabled"
+        : "bg-quaternary text-primary";
+    case "tertiary":
+      break;
+
+    default:
+      return disabled ? "" : "";
+  }
+}
+
+const Button = ({
+  variant = "primary",
+  children,
+  className,
+  disabled,
+  ...rest
+}: ButtonProps) => {
+  return (
     <button
       {...rest}
-      className={`${classes} ${generalStyles} ${className}`}
+      className={`rounded-md px-6 py-2 ${getVariant(
+        variant,
+        disabled
+      )} ${className}`}
       disabled={disabled}
     >
       {children}
     </button>
   );
-
-  return Btn(disabled ? "bg-disabled text-disabled" : "bg-primary text-white");
 };
 
 export default Button;
